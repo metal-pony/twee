a0ae9727c3db97f4d8c6058daef7cfc984c34424
Jeff <metal-pony@users.noreply.github.com>
2025-03-28 20:23:39 -0400

    Remove fluff

--- src/components/page-apps/Game.jsx
+++ src/components/page-apps/Game.jsx
@@ -52,6 +52,7 @@ function drawPreviewPixel(previewCanvasRef, x, y, color) {
   if (previewCanvasRef.current) {
     const previewCanvas = previewCanvasRef.current;
     const previewCtx = previewCanvas.getContext('2d');
+    previewCtx.clearRect(x, y, 1, 1);
     previewCtx.fillStyle = color;
     previewCtx.fillRect(x, y, 1, 1);
   }
@@ -60,21 +61,12 @@ function drawPreviewPixel(previewCanvasRef, x, y, color) {
 /**
  *
  * @param {object} props
- * @returns
  */
 export function Game({}) {
-  console.log('Game component loading');
-
-  /**
-   * @type {React.RefObject<HTMLCanvasElement>}
-   */
+  /** @type {React.RefObject<HTMLCanvasElement>}*/
   const canvasRef = useRef(null);
-  /**
-   * @type {React.RefObject<HTMLCanvasElement>}
-   */
+  /** @type {React.RefObject<HTMLCanvasElement>}*/
   const previewCanvasRef = useRef(null);
-  /** @type {string} */
-  const [capturedImgData, setCapturedImgData] = useState(null);
   const [selectedColor, setSelectedColor] = useState('#000000');
   const [cellSize, setCellSize] = useState(scene.cellSize);
   const [gridWidth, setGridWidth] = useState(scene.cols);
@@ -82,34 +74,15 @@ export function Game({}) {
   const [showGrid, setShowGrid] = useState(scene.showGrid);
 
   useEffect(() => {
-    console.log('useEffect loading');
-
     const canvas = canvasRef?.current;
-
-    if (!canvas) {
-      throw new Error('Canvas not found. Cannot initialize engine.');
-    }
-
     let isDragging = false;
-    /**
-     *
-     * @param {MouseEvent} ev
-     */
+
     canvas.onclick = (ev) => {
-      // When control is held, restart the scene
+      // When SHIFT is held, restart the scene
       if (ev.shiftKey) {
         console.log('Restarting scene');
-        if (engine.status === 'IN_PROGRESS') {
-          engine.stop();
-        }
         engine.resetScene();
       } else {
-        // if (engine.isRunning) {
-        //   engine.stop();
-        // } else {
-        //   engine.start();
-        // }
-
         // Get the cell that was clicked
         const rect = canvas.getBoundingClientRect();
         const x = bounded(Math.floor((ev.clientX - rect.left) / scene.cellSize), 0, scene.cols - 1);
@@ -124,22 +97,12 @@ export function Game({}) {
       }
     }
 
-    /**
-     *
-     * @param {MouseEvent} ev
-     */
     window.onmousedown = (ev) => {
-      console.log('(window) Mouse down');
-      if (ev.button === 0) {
-        isDragging = true;
-      }
+      if (ev.button === 0) isDragging = true;
     }
-    // Also stop dragging when the mouse is released outside the canvas
+    // Stop dragging when the mouse is released outside the canvas
     window.onmouseup = (ev) => {
-      console.log('(window) Mouse up');
-      if (ev.button === 0) {
-        isDragging = false;
-      }
+      if (ev.button === 0) isDragging = false;
     }
 
     // When the mouse moves, if dragging, set the color of the cell that is hovered over
@@ -148,11 +111,7 @@ export function Game({}) {
         const rect = canvas.getBoundingClientRect();
         const x = bounded(Math.floor((ev.clientX - rect.left) / scene.cellSize), 0, scene.cols - 1);
         const y = bounded(Math.floor((ev.clientY - rect.top) / scene.cellSize), 0, scene.rows - 1);
-        console.log(`Dragging over cell (${x}, ${y})`);
-
-        // Set the cell color
         scene.cellData(x, y).color = selectedColor;
-        console.log(`Set cell (${x}, ${y}) to color ${selectedColor}`);
         drawPreviewPixel(previewCanvasRef, x, y, selectedColor);
         engine.render();
       }
@@ -172,6 +131,7 @@ export function Game({}) {
     };
   }, [selectedColor]);
 
+  const paletteColorsPerRow = 6;
   const palette = [
     '#172038','#253a5e','#3c5e8b','#4f8fba','#73bed3','#a4dddb',
     '#19332d','#25562e','#468232','#75a743','#a8ca58','#d0da91',
@@ -182,8 +142,7 @@ export function Game({}) {
     '#090a14','#10141f','#151d28','#202e37','#394a50','#577277',
     '#819796','#a8b5b2','#c7cfcc','#ebede9','#00000000'
   ].reduce((acc, color, i) => {
-    // Group into rows of 12
-    if (i % 6 === 0) {
+    if (i % paletteColorsPerRow === 0) {
       acc.push([]);
     }
     acc[acc.length - 1].push(color);
@@ -199,12 +158,7 @@ export function Game({}) {
 
   return (
     <Page includeGoats={false} includeNav={false}>
-      <div
-        className='pt----- flex-horizontal col-gap--'
-        // style={{
-        //   alignItems: 'center',
-        // }}
-      >
+      <div className='pt----- flex-horizontal col-gap--'>
         {/* Left-side controls panel */}
         <div className='flex-vertical row-gap--'>
           {/* Grid Size */}
@@ -325,27 +279,6 @@ export function Game({}) {
               />
             </div>
           </div>
-
-          {
-            /* Preview image */
-            capturedImgData && capturedImgData.length > 0 && (
-              <div
-                className='inline-flex-horizontal col-gap--'
-                style={{ alignItems: 'start' }}
-              >
-                <img src={capturedImgData}  />
-                {/* Icon of garbage bin that deletes the image when clicked */}
-                <button
-                  className='p- btn bg-dark grey border-2 border-rounded-2 border-grey hoverable hover-evil shadow'
-                  onClick={() => {
-                    setCapturedImgData(null);
-                  }}
-                >
-                  <i className='fas fa-trash' />
-                </button>
-              </div>
-            )
-          }
         </div>
       </div>
     </Page>
